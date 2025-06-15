468ebcfed25357da219adf4f45257e2a90316347
Pixelated <fabian.tomczyk@posteo.de>
2025-06-15 11:58:32 +0200

    Added function to split recorded audio into 10 second chunks using ffmpeg

--- Record.py
+++ Record.py
@@ -7,9 +7,16 @@ import usb.core
 import usb.util
 import sys
 import time
+import subprocess
 
 
 class Record:
+    """Class for recording audio with a ReSpeaker Mic Array from Seeed Studio
+
+    Using tuning.py provided by Seeed Studio this class allows to record audio
+    continously until CONTINUERECORDING is set to false.
+    A signal handler can call stop_Recording() to stop the recording.
+    """
     
     # Recording Parameters
     FACTOR = 2
@@ -18,7 +25,7 @@ class Record:
     RESPEAKER_WIDTH = 2
     RESPEAKER_INDEX = 11
     CHUNK = 1024 * FACTOR
-    millisecondsChunk = 64 * FACTOR
+    millisecondsChunk = 64 * FACTOR 
     MIC_ARRAY_NAME = "ReSpeaker 4 Mic Array (UAC1.0)"
     #RECORD_SECONDS = 10
     
@@ -43,18 +50,22 @@ class Record:
         
         self.mic_index = self.get_Mic()
         
-        self.stream = self.p.open(
-            rate=self.RESPEAKER_RATE,
-            format=self.p.get_format_from_width(self.RESPEAKER_WIDTH),
-            channels=self.RESPEAKER_CHANNELS,
-            input=True,
-            input_device_index=self.mic_index
-        ) 
+        try:
+            self.stream = self.p.open(
+                rate=self.RESPEAKER_RATE,
+                format=self.p.get_format_from_width(self.RESPEAKER_WIDTH),
+                channels=self.RESPEAKER_CHANNELS,
+                input=True,
+                input_device_index=self.mic_index
+            ) 
+        except:
+            print("Couldn't open audio stream. Check if Microphone properly connected.")
+            sys.exit(1)
 
         try:
             self.dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
         except:
-            print("Couldn't find mic for voice activity and angle detection!")
+            print("Couldn't find mic for voice activity and angle detection.")
             sys.exit(1)
 
         self.voice_activity = Tuning(self.dev)
@@ -70,6 +81,8 @@ class Record:
 
 
     def record_With_Voice_Activity(self):
+        """Record only when mic detected voice activity."""
+
         fileIndexName = 0
         frames = []
         duration = 0
@@ -90,24 +103,26 @@ class Record:
         fileIndexName += 1
 
 
-    def record_only_during_Voice_Activity(self):
-        fileIndexName = 0
-        frames = []
+    #def record_only_during_Voice_Activity(self):
+    #    fileIndexName = 0
+    #    frames = []
 
-        while self.CONTINUERECORDING:
-            if self.voice_activity.is_voice():
-                data = self.stream.read(self.CHUNK, exception_on_overflow=False)
-                frames.append(data)
-            elif len(frames) > 0:
-                self.write_File(frames, fileIndexName)
-                fileIndexName += 1
+    #    while self.CONTINUERECORDING:
+    #        if self.voice_activity.is_voice():
+    #            data = self.stream.read(self.CHUNK, exception_on_overflow=False)
+    #            frames.append(data)
+    #        elif len(frames) > 0:
+    #            self.write_File(frames, fileIndexName)
+    #            fileIndexName += 1
 
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
+    #    self.stream.stop_stream()
+    #    self.stream.close()
+    #    self.p.terminate()
         
         
     def record_till_Interrupt(self):
+        """Record continously."""
+
         fileIndexName = 0
         frames = []
 
@@ -125,6 +140,8 @@ class Record:
 
 
     def write_File(self, frames, fileIndexName):
+        """Write recording to a file with an index."""
+
         wf = wave.open(self.OUTPUT_DIR + self.WAVE_OUTPUT_FILENAME + str(fileIndexName) + ".wav", "wb")
         wf.setnchannels(self.RESPEAKER_CHANNELS)
         wf.setsampwidth(self.p.get_sample_size(self.p.get_format_from_width(self.RESPEAKER_WIDTH)))
@@ -138,14 +155,48 @@ class Record:
 
 
     def angle_Detection(self):
-        while self.CONTINUERECORDING:
-            self.voice_direction = self.voice_activity.direction
-            print("DOA: " + str(self.voice_activity.direction))
-            time.sleep(1)
+        self.voice_direction = self.voice_activity.direction
+        #print("DOA: " + str(self.voice_activity.direction))
         return self.voice_activity.direction
 
 
+    def write_Angle(self):
+        timer = 0
+        f = open("./Recs/angle.txt", "w")
+
+        while self.CONTINUERECORDING:
+            angle = self.angle_Detection()
+            f.write(str(timer) + " " + str(angle) + "\n")
+            time.sleep(1)
+            timer += 1
+
+        f.close()
+            
+
+
+    def split_Audio(self):
+        split_intervall_seconds = 10
+
+        commands = [
+            "ffmpeg",
+            "-i", "./Recs/recording0.wav",
+            "-acodec", "copy",
+            "-f", "segment",
+            "-segment_time", "10",
+            "-reset_timestamps", "1",
+            "-map", "0",
+            "./Recs/chunk%03d.wav"
+            ]
+
+        if subprocess.run(commands).returncode == 0:
+            print("Split erfolgreich")
+        else:
+            print("Split fehlgeschlagen")
+
     def record_single_Chunk(self):    
+        """Only used for test purposes. To determine how many milliseconds
+        one CHUNK equal to.
+        """
         data = self.stream.read(self.CHUNK, exception_on_overflow=False)
         frames = []
         frames.append(data)
@@ -155,3 +206,6 @@ class Record:
         self.p.terminate()
 
         self.write_File(frames, 420)
+
+
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 def signalHandler(signum, frame):
     print("Stopping")
-    Rec.stopRecording()
+    Rec.stop_Recording()
     Log.stop_Log()
 
 
@@ -18,11 +18,12 @@ Log = Logging(os.getpid())
 
 signal.signal(signal.SIGINT, signalHandler)
 
+#RecThread = threading.Thread(target=Rec.record_10_second_Chunks)
 RecThread = threading.Thread(target=Rec.record_till_Interrupt) 
 #RecThread = threading.Thread(target=Rec.record_With_Voice_Activity) 
 #RecThread = threading.Thread(target=Rec.record_only_during_Voice_Activity) 
 LogThread = threading.Thread(target=Log.start_Log)
-DOAThread = threading.Thread(target=Rec.angle_Detection)
+DOAThread = threading.Thread(target=Rec.write_Angle)
 
 RecThread.start()
 LogThread.start()
@@ -31,3 +32,5 @@ DOAThread.start()
 RecThread.join()
 LogThread.join()
 DOAThread.join()
+
+Rec.split_Audio()
